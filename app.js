941078f440eff0d6cd7ce85df70f8fee328139ab
obeza <o.bezaudin@gmail.com>
2016-06-03 15:30:14 -0400

    ajout d'events

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.controller('MyController', function($scope) {
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    console.log('start : ' + new Date(y, m, 1));
+    var moisActuel = "";
+
+    //console.log('start : ' + moment('2016-06-15'));
 
     function formatDate(aaaa,m,j){
     	m = m - 1;
@@ -17,6 +19,7 @@ app.controller('MyController', function($scope) {
 
 	var events = [
 					{
+						id:123,
 						title: 'All Day Event',
 						start  : new Date('Wed Jun 10 2016 00:00:00 GMT-0400 (AST)'),
 						allDay:false
@@ -25,31 +28,44 @@ app.controller('MyController', function($scope) {
 
 	$scope.eventSources = [events];
 
-    $scope.alertEventOnClick = function( date, jsEvent, view){
+    $scope.alertDayOnClick = function( date, jsEvent, view){
     	console.log('click ');
+    	var jourMoment = moment(date.format())._d;
 
-    	console.log( JSON.stringify(view.name) );
+    	console.log( jourMoment );
 
+    };
+
+    $scope.alertOnEventClick = function( date, jsEvent, view){
+        console.log(date);
     };
 
     $scope.changeView = function (view, element) {
             
-            console.log( view.start._d );
-            console.log( view.end._d );
+            console.log( "current : " + view.title );
+            console.log( "start : " + view.start._d );
+            console.log( "end : " + view.end._d );
+
+            if (moisActuel){
+            	console.log('deja data');
+            } else {
+            	moisActuel = view.title;
+            }
         };
 
     /* config object */
     $scope.uiConfig = {
       calendar:{
       	lang: 'fr',
-        height: 450,
+        height: 500,
         editable: true,
         header:{
           left: 'month agendaWeek agendaDay',
           center: 'title',
           right: 'today prev,next'
         },
-        dayClick: $scope.alertEventOnClick,
+        dayClick: $scope.alertDayOnClick,
+        eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
         viewRender:$scope.changeView,
